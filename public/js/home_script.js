715ed297f661c35ef7b5b638e4fb800103b64f90
MathisJean <mathisojean@hotmail.com>
2025-09-18 19:52:04 -0300

    Save Local Edits

--- public/js/home_script.js
+++ public/js/home_script.js
@@ -8,3 +8,24 @@ slide_index = 4;
 
 //Initial Slideshow render
 go_to_slide(slides[3]);
+
+feature_cards = feature_cards.filter((_, index) => index !== 3);
+
+function click_handler(event)
+{
+    event.preventDefault();
+
+    hint_click.style.animation = "none"; //Stop Animation
+    void hint_click.offsetHeight; //Force Reflow
+    hint_click.style.opacity = 0;
+
+    feature_cards.forEach(card => 
+    {
+        card.removeEventListener("mousedown", click_handler, { passive: false }); //Remove Event Listener
+    });
+}
+
+feature_cards.forEach(card => 
+{
+    card.addEventListener("mousedown", click_handler, { passive: false }); //Remove Event Listener
+});
--- public/js/projects_script.js
+++ public/js/projects_script.js
@@ -2,10 +2,31 @@
 const root = document.documentElement;
 const color = getComputedStyle(root).getPropertyValue('--accent-yellow').trim();
 
-slide_index = 3;
+slide_index = 2;
 
 //Initial Slideshow render
 go_to_slide(slides[2]);
+
+feature_cards = feature_cards.filter((_, index) => index !== 1);
+
+function click_handler(event)
+{
+    event.preventDefault();
+
+    hint_click.style.animation = "none"; //Stop Animation
+    void hint_click.offsetHeight; //Force Reflow
+    hint_click.style.opacity = 0;
+
+    feature_cards.forEach(card => 
+    {
+        card.removeEventListener("mousedown", click_handler, { passive: false }); //Remove Event Listener
+    });
+}
+
+feature_cards.forEach(card => 
+{
+    card.addEventListener("mousedown", click_handler, { passive: false }); //Remove Event Listener
+});
 
 if(mobile_media_query.matches)
 {
